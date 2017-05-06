e37ac4af492dee002ace89fcc4cf031a866a3129
Jeremy Live <jeremylive@live.com>
2017-05-06 10:44:42 -0600

    main, llama a interfaz

--- src/storyteller/program/Main.java
+++ src/storyteller/program/Main.java
@@ -1,4 +1,6 @@
 package storyteller.program;
+import java.awt.EventQueue;
+import storyteller.interfaz.Interfaz;
 /*
  * Clase Main
  * 
@@ -7,23 +9,17 @@ public class Main {
 	/*Main*/
 	public static void main(String[] args)
 	{
-		//------------------
-		//Inicio el programa
-		//------------------
-
-        /*
-        *
-        *
-        *
-        */
-/*
-            
-            
-            en esta funcion voy a hacer algo nuevo
-            */
-
-
-
+	//Inicio el programa Psicodelic canvas
+	  EventQueue.invokeLater(new Runnable() {
+	        public void run() {
+	            try {	
+	            	Interfaz jFrame = new Interfaz();
+	                jFrame.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }); 
 		//-------------------		
 		//Termino el programa
 		//-------------------
