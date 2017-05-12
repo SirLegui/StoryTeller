581775c7f2618c10344516ca3d402a3387857322
SirLegui <edgerik.leguizamon@gmail.com>
2017-05-12 02:27:38 -0600

    Codigo completo para manejo de imagenes

--- src/storyteller/librerias/MCS.java
+++ src/storyteller/librerias/MCS.java
@@ -2,14 +2,18 @@ package storyteller.librerias;
 
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Iterator;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
@@ -22,18 +26,15 @@ import org.json.simple.parser.ParseException;
 
 public class MCS
 {
-    public static void main(String[] args)
+    public String[] getDescription(String url)
     {
         HttpClient httpclient = HttpClients.createDefault();
-
+        String[] rets = null;
         try
         {
-            //Editor : Edgerik
-            URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze");
+            URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe");
 
-            builder.setParameter("visualFeatures", "Categories");
-            builder.setParameter("details", "{string}");
-            builder.setParameter("language", "en");
+            builder.setParameter("maxCandidates", "1");
 
             URI uri = builder.build();
             HttpPost request = new HttpPost(uri);
@@ -42,71 +43,100 @@ public class MCS
 
 
             // Request body
-            StringEntity reqEntity = new StringEntity("{\"url\":\"http://viajar.especiales.elperiodico.com/50-viajes-extraordinarios/files/2013/05/selva-del-tigre.jpg\"}");
+            StringEntity reqEntity = new StringEntity("{\"url\":\""+url+"\"}");
             request.setEntity(reqEntity);
 
             HttpResponse response = httpclient.execute(request);
             HttpEntity entity = response.getEntity();
 
-
-
-            if (entity != null)
+            if (entity != null) 
             {
-                System.out.println(EntityUtils.toString(entity));
+                System.out.println("Se ha obtenido una respuesta valida");
             }
+            rets = funcionJson(EntityUtils.toString(entity));
         }
-        catch (Exception e)
+        catch (URISyntaxException | IOException | org.apache.http.ParseException | ParseException e)
         {
             System.out.println(e.getMessage());
         }
+        return rets;
     }
 
 
 
 
+    public void getImage(String urli) throws MalformedURLException, FileNotFoundException, IOException{
+        // Url con la foto
+        try{
+            URL url = new URL(urli);
+
+            // establecemos conexion
+            URLConnection urlCon = url.openConnection();
+
+            // Sacamos por pantalla el tipo de fichero
+            System.out.println(urlCon.getContentType());
+
+            // Se obtiene el inputStream de la foto web y se abre el fichero
+            // local.
+            InputStream is = urlCon.getInputStream();
+            FileOutputStream fos = new FileOutputStream("/home/edgerik/foto1.jpg");
+
+            // Lectura de la foto de la web y escritura en fichero local
+            byte[] array = new byte[1000]; // buffer temporal de lectura.
+            int leido = is.read(array);
+            while (leido > 0) {
+                    fos.write(array, 0, leido);
+                    leido = is.read(array);
+            }
+
+            // cierre de conexion y fichero.
+            is.close();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /*
      *
      *
      */
-    public void funcionJson() throws ParseException
+    public String[] funcionJson(String json) throws ParseException
     {
+        String[] rets = {"","","",""};
 
         JSONParser parser = new JSONParser();
 
-        try {
+        Object obj = parser.parse(json);
 
-            Object obj = parser.parse(new FileReader("c:\\FileTests\\prueba.json"));
+        JSONObject jsonObject = (JSONObject) obj;
 
-            JSONObject jsonObject = (JSONObject) obj;
+        JSONObject descripcion = (JSONObject) jsonObject.get("description");
 
-            String blog = (String) jsonObject.get("Blog");
-            System.out.println(blog);
+        // loop array
+        JSONArray tags = (JSONArray) descripcion.get("tags");
+        Iterator<String> iterator = tags.iterator();
 
-            String temas = (String) jsonObject.get("Temas");
-            System.out.println(temas);
-
-            long inicio = (Long) jsonObject.get("Inicio");
-            System.out.println(inicio);
-
-            JSONObject innerObject = (JSONObject) jsonObject.get("Posts");
-            System.out.println(innerObject.toJSONString());
-
-            // loop array
-            JSONArray tags = (JSONArray) jsonObject.get("Tags");
-            Iterator<String> iterator = tags.iterator();
-
-            while (iterator.hasNext()) {
-                    System.out.println(iterator.next());
+        JSONArray caption = (JSONArray) descripcion.get("captions");
+        Iterator<JSONObject> iter = caption.iterator();
+        JSONObject text = iter.next();
+        String texto = (String) text.get("text");
+        rets[0] = texto;
+        for(int i = 1; i<4;i++)
+        {
+            if(iterator.hasNext()){
+                rets[i]= iterator.next();
             }
-        } catch (FileNotFoundException e) {
-                //manejo de error
-        }catch (IOException e) {
-                //manejo de error
         }
+        
+        
+        System.out.println(texto);
+        
+        
     //manejo de error
-
+        return rets;
     }
+    
 
 
 }
