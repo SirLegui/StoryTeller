c2dd0df546c101afdb46549cd48f3ffba7e99f2b
SirLegui <edgerik.leguizamon@gmail.com>
2017-05-12 02:29:46 -0600

    Main corregido con llamada a nueva interfaz

--- src/storyteller/program/Main.java
+++ src/storyteller/program/Main.java
@@ -1,6 +1,8 @@
 package storyteller.program;
 import java.awt.EventQueue;
-import storyteller.interfaz.Interfaz;
+//import storyteller.interfaz.Interfaz;
+import storyteller.interfaz.Ventana_Main;
+import storyteller.librerias.MCS;
 /*
  * Clase Main
  * 
@@ -10,16 +12,20 @@ public class Main {
 	public static void main(String[] args)
 	{
 	//Inicio el programa Psicodelic canvas
-	  EventQueue.invokeLater(new Runnable() {
-	        public void run() {
-	            try {	
-	            	Interfaz jFrame = new Interfaz();
-	                jFrame.setVisible(true);
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }); 
+	EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {	
+                    //String prueba = "{\"description\":{\"tags\":[\"cat\",\"laying\",\"animal\",\"indoor\",\"mammal\",\"lying\",\"top\",\"bed\",\"sitting\",\"sleeping\",\"orange\",\"blanket\",\"white\",\"grey\",\"blue\",\"red\",\"playing\"],\"captions\":[{\"text\":\"a cat lying on a bed\",\"confidence\":0.9485613629872266}]},\"requestId\":\"d682908f-ad5d-434f-9fb8-f948784e141c\",\"metadata\":{\"width\":460,\"height\":260,\"format\":\"Jpeg\"}}";
+
+//                    Interfaz jFrame = new Interfaz();
+//                    jFrame.setVisible(true);
+                    Ventana_Main v1 = new Ventana_Main();
+                    v1.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }); 
 		//-------------------		
 		//Termino el programa
 		//-------------------
