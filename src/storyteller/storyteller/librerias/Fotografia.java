141cd98d3408f829a0f9d6777150785cf2abb55a
SirLegui <edgerik.leguizamon@gmail.com>
2017-05-17 17:11:41 -0600

    Base para fotografia

---
+++ src/storyteller/storyteller/librerias/Fotografia.java
@@ -0,0 +1,14 @@
+package storyteller.librerias;
+
+import java.awt.Image;
+
+public class Fotografia {
+
+    private Image imagen;
+    private String caption;
+    private String url;
+    
+    
+    
+    
+}
