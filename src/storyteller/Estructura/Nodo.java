f498b503072a2d4fae9295482c8d7edbc26f2311
Jeremy Live <jeremylive@live.com>
2017-05-17 14:55:51 -0600

    Codigo Nodo, falta que hacer mas

--- src/storyteller/Estructura/Nodo.java
+++ src/storyteller/Estructura/Nodo.java
@@ -1,5 +1,23 @@
 package storyteller.Estructura;
+/*Importes de bibliotecas*/
 
-public class Nodo {
-
+/*
+ * Clase Nodo generica
+ */
+public class Nodo<T> {
+    /*Variables Globales*/
+    int key, height;
+    Nodo<T> left, right;
+    /*Constructores*/
+    public Nodo()
+    {
+        
+    }
+    public Nodo(int d) 
+    {
+        key = d;
+        height = 1;
+    }
+    
+    //Fin..
 }
