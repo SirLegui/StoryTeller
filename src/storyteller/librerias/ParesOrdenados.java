e715fe9d60fa0ae009fb9b3974062f7c61c39a26
Jeremy Live <jeremylive@live.com>
2017-05-23 01:29:46 -0600

    Creo interfaz2
    -Empiza la programada en pares Ordenados jajaja ya son la 1:30am 23/05/17 XD

--- src/storyteller/librerias/ParesOrdenados.java
+++ src/storyteller/librerias/ParesOrdenados.java
@@ -1,20 +1,36 @@
 package storyteller.librerias;
-
+/*Librerias a usar*/
 import java.io.Serializable;
 import java.util.ArrayList;
+/*
+Clase de Pares ordenados
 
-public class ParesOrdenados implements Serializable{
-    
-    private ArrayList<String> indices;
-    
-    
-    /**
-     * [nombre del album,   direccion]
-     * bqwe              0
-     * aass              500
-     */
-
-    public ParesOrdenados() {
+Implemento Serializacion para serializar el array de strings de pares ordenados
+*/
+public class ParesOrdenados implements Serializable
+{
+    // Variables globales 
+    private String[][] indices;
+    // Constructor
+    public ParesOrdenados() 
+    {
+        this.indices = null;
     }
-    
+    // Gets ands Sets
+    public String[][] getIndices()
+    {
+        return indices;
+    }
+    public void setIndices(String[][] pIndices)
+    {
+        indices = pIndices;
+    }
+    // Funciones de PäresOrdenados
+    // Funcion que convierte el String[][] a un byte[][] Serializado
+    public void conviertoBytes()
+    {
+        
+    }
+    // ->
+   
 }
--- src/storyteller/program/Serializacion.java
+++ src/storyteller/program/Serializacion.java
@@ -1,5 +1,5 @@
 package storyteller.program;
-
+/*Librerias a usar*/
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -7,69 +7,41 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+/*
+Clase Serializacion 
 
-public class Serializacion<e> {
-	
-        public byte[] LeerBytes(String dire)
-    {
+Generica, serializa un byte[]
+*/
+public class Serializacion<e> 
+{
 
-        FileInputStream fileInputStream = null;
-        File file = new File(dire);
-
-        byte[] fileArray = new byte[(int) file.length()];
-        
-
+  public byte[] serializar(e objeto){
+        ByteArrayOutputStream bs= new ByteArrayOutputStream();
+        ObjectOutputStream os;
         try {
-            // Con este código se obtienen los bytes del archivo.
-            fileInputStream = new FileInputStream(file);
-            fileInputStream.read(fileArray);
-
-            fileInputStream.close();
-
-//            // Con este código se agregan los bytes al archivo.
-//            FileOutputStream fileOuputStream = new FileOutputStream("C:\\ejemplo.txt");
-//            fileOuputStream.write(fileArray);
-//            fileOuputStream.close();
-
-//            for (int i = 0; i < fileArray.length;i++)
-//            {
-//                    System.out.println(fileArray[i]);
-//            }
-            System.out.println("Tamaño de archivo = "+fileArray.length);
-
-            
-        } catch (Exception e) {
-                //Manejar Error
+                os = new ObjectOutputStream (bs);
+                os.writeObject(objeto);
+                os.close();
+        } catch (IOException e1) {
+                e1.printStackTrace();
         }
-        return fileArray;
-    }	
-	public byte[] serializar(e objeto){
-		ByteArrayOutputStream bs= new ByteArrayOutputStream();
-		ObjectOutputStream os;
-		try {
-			os = new ObjectOutputStream (bs);
-			os.writeObject(objeto);
-			os.close();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		return bs.toByteArray();
-	}
-	
-	public e deserializar(byte[] bytes){
-		ByteArrayInputStream bs= new ByteArrayInputStream(bytes);
-		ObjectInputStream is;
-		e objeto = null;
-		try {
-			is = new ObjectInputStream(bs);
-			objeto = (e)is.readObject();
-			is.close();
-		} catch (IOException | ClassNotFoundException e1) {
-                        e1.printStackTrace();
-			System.out.println("Se ha ingresado un archivo con una extension invalida");
-                        System.out.println("El archivo leido probablemente es de una version anterior del programa");
-                }
-		
-		return objeto;
-	}
+        return bs.toByteArray();
+    }
+
+    public e deserializar(byte[] bytes){
+        ByteArrayInputStream bs= new ByteArrayInputStream(bytes);
+        ObjectInputStream is;
+        e objeto = null;
+        try {
+                is = new ObjectInputStream(bs);
+                objeto = (e)is.readObject();
+                is.close();
+        } catch (IOException | ClassNotFoundException e1) {
+                e1.printStackTrace();
+                System.out.println("Se ha ingresado un archivo con una extension invalida");
+                System.out.println("El archivo leido probablemente es de una version anterior del programa");
+        }
+
+        return objeto;
+    }
 }
