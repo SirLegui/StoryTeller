08e40eac8ace0477e45317f4431b5291c0ce6620
Jeremy Live <jeremylive@live.com>
2017-05-24 11:39:56 -0600

    Comente

--- src/storyteller/librerias/Album.java
+++ src/storyteller/librerias/Album.java
@@ -1,42 +1,63 @@
 package storyteller.librerias;
-
+/*Librerias a usar*/
 import storyteller.Estructura.ArbolAVL;
-
-public class Album {
+/**
+ * 
+ * @author live, edgerik
+ * 
+ * Clase Album
+ * 
+ * AVL,name,c\\Direccion.x
+ */
+public class Album 
+{
+    // Variables Globales --------------------------------------------------
     private ArbolAVL tree;
     private String nombre;
     private String local_path;
-
-    public Album(ArbolAVL tree, String nombre, String local_path) {
+    /**
+     * Constructor ---------------------------------------------------------
+     * @param tree
+     * @param nombre
+     * @param local_path 
+     */
+    public Album(ArbolAVL tree, String nombre, String local_path) 
+    {
+        // 
         this.tree = tree;
         this.nombre = nombre;
         this.local_path = local_path;
     }
-
-    public String getNombre() {
+    /**
+     * Gets and Sets
+     */
+    public String getNombre() 
+    {
         return nombre;
     }
-
-    public String getLocal_path() {
+    
+    public String getLocal_path() 
+    {
         return local_path;
     }
-
-    public ArbolAVL getTree() {
+    
+    public ArbolAVL getTree() 
+    {
         return tree;
     }
-
-    public void setLocal_path(String local_path) {
+    
+    public void setLocal_path(String local_path) 
+    {
         this.local_path = local_path;
     }
-
-    public void setNombre(String nombre) {
+    
+    public void setNombre(String nombre) 
+    {
         this.nombre = nombre;
     }
 
-    public void setTree(ArbolAVL tree) {
+    public void setTree(ArbolAVL tree) 
+    {
         this.tree = tree;
-    }
-    
-    
-    
+    } 
 }
